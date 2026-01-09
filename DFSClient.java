c5d74ea9b401bf6ad704da89c138bb014de95ff6
xytlw <142857@ruri.waseda.jp>
2026-01-09 14:39:02 +0000

    update

--- DFSClient.java
+++ DFSClient.java
@@ -17,11 +17,8 @@ public class DFSClient {
             //client.open("localhost", "sample.txt", "RW");
             // Read current content
             //System.out.println("Current cache: " + client.read());
-            // Rewrite conte
-
-            //System.out.println("Test completed");
-
-            
+            // Rewrite content
+            //System.out.println("Test completed");            
             // Open file with read-write mode(rw)
             Scanner scanner = new Scanner(System.in);
             
@@ -33,24 +30,25 @@ public class DFSClient {
                 
                 if (parts.length == 0) continue;
                 String cmd = parts[0].toLowerCase();
+
                 switch (cmd) {
                     case "open":
                         if (parts.length >= 4) {
                             client.open(parts[1], parts[2], parts[3]);
                         } else {
-                        System.out.println("Usage: open <host> <path> <mode>");
+                        System.out.println("Format: open <host> <path> <mode>");
                         }
                         break;
 
                     case "read":
                         System.out.println("Current cache: " + client.read());
                         break;
+
                     case "write":
                         if (parts.length >= 2) {
                             client.write(parts[1]);
-                            //System.out.println("Data written.");
                         } else {
-                            System.out.println("Usage: write <content>");
+                            System.out.println("Format: write <content>");
                         }
                         break;
                 
@@ -58,56 +56,25 @@ public class DFSClient {
                         client.close();
                         break;
 
-                    case "-ls":
+                    case "ls":
                         client.listFiles();
                         break;
 
-                    case "view_locks":
+                    case "vlock":
                         client.viewLocks();
                         break;
 
                     case "exit":
                         System.out.println("Test completed");
                         return;
+
                     default:
-                        System.out.println("Unknown command. Available: open, read, write, close, exit");
+                        System.out.println("Unknown command. Available: open, read, write, close, exit, ls, vlock");
             }
         }
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
             e.printStackTrace();
-        }
-    }
-
-    public void listFiles() throws IOException {
-        try (Socket socket = new Socket(serverHost, serverPort);
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-
-            out.println("-ls");
-            String line;
-            System.out.println("Files on server:");
-            while (!(line = in.readLine()).equals("FILES_LIST_END")) {
-                if (!line.equals("FILES_LIST_START")) {
-                    System.out.println(" - " + line);
-                }
-            }
-        }
-    }
-
-    public void viewLocks() throws IOException {
-        try (Socket socket = new Socket(serverHost, serverPort);
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-
-            out.println("VIEW_LOCKS");
-            String line;
-            System.out.println("Current Locks on server:");
-            while (!(line = in.readLine()).equals("LOCKS_LIST_END")) {
-                if (!line.equals("LOCKS_LIST_START")) {
-                    System.out.println(" - " + line);
-                }
-            }
         }
     }
 
@@ -120,6 +87,7 @@ public class DFSClient {
         try (Socket socket = new Socket(serverHost, serverPort);
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
 
             out.println("FETCH " + path + " " + mode);
             String status = in.readLine();
@@ -159,17 +127,22 @@ public class DFSClient {
             try (Socket socket = new Socket(serverHost, serverPort);
                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                
 
                 if(isDirty){
+                   
                     out.println("STORE " + currentFilePath);
                     out.print(localCache + "\n__END__\n");
                     out.flush();
 
-                    if ("SUCCESS".equals(in.readLine())) {
+                    String w = in.readLine();
+                    System.out.println("Server response: " + w);
+
+                    if ("SUCCESS".equals(w)) {
                         System.out.println("Changes saved to server.");
                     }
                 }else{
-                    out.println("UNLOCK " + currentFilePath);
+                    out.println("UNLOCK " + currentFilePath);//ロック解除だけ行う
                     if ("SUCCESS".equals(in.readLine())) System.out.println("No changes to save.");
                 }
         
@@ -179,5 +152,44 @@ public class DFSClient {
         this.localCache = null;
         this.currentFilePath = null;
         System.out.println("Client closed.");
+    }
+
+    // listFiles: サーバ上のファイル一覧を取得
+    public void listFiles() throws IOException { 
+        try (Socket socket = new Socket(serverHost, serverPort);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            out.println("LIST_FILES");//コマンドをサーバへ送信
+            
+            String line;
+            
+            while (!(line = in.readLine()).equals("FILES_LIST_END")) { 
+                if (!line.equals("FILES_LIST_START")) {
+                    System.out.println(line);
+                }
+            }
+
+        }
+    }
+
+    // viewLocks: サーバ上のファイルのロック状態を取得
+    public void viewLocks() throws IOException { 
+        try (Socket socket = new Socket(serverHost, serverPort);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            out.println("VIEW_LOCKS");//コマンドをサーバへ送信
+
+            String line;
+            System.out.println("Current Locks on server:");
+
+            while (!(line = in.readLine()).equals("LOCKS_LIST_END")) {
+                if (!line.equals("LOCKS_LIST_START")) {
+                    System.out.println(" - " + line);
+                }
+            }
+
+        }
     }
 }
--- DFSServer.java
+++ DFSServer.java
@@ -2,13 +2,19 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.util.concurrent.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DFSServer {
     private static final int PORT = 8080;
     // ファイル内容をメモリ上で管理
-    private static Map<String, String> fileStore = new ConcurrentHashMap<>();
+    private static Map<String, File> fileStore = new ConcurrentHashMap<>();
     // 書き込み制限(同時書き込みを防ぐ)
     private static Map<String, String> writeLocks = new ConcurrentHashMap<>();
+
+    private static enum modeSet { //アクセスモードの集合
+        READ_ONLY, WRITE_ONLY, RW
+    }
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(PORT);
@@ -17,6 +23,18 @@ public class DFSServer {
         while (true) {
             Socket clientSocket = serverSocket.accept();
             new Thread(new ClientHandler(clientSocket)).start();
+        }
+    }
+
+    static class File{// ファイル情報を管理するクラス
+        String content; // ファイル内容
+        LocalDateTime lastsaved; // 最終保存日時(closeにより更新された時間)
+        LocalDateTime lastaccessed; // 最終アクセス日時(openした時間)
+
+        public File() {
+            this.content = "";
+            this.lastsaved = null;
+            this.lastaccessed = LocalDateTime.now();
         }
     }
 
@@ -42,6 +60,17 @@ public class DFSServer {
                     String path = parts[1];
                     String mode = parts[2];
 
+                    while(true){//期待されるmodeでなければerrorを返す
+                        try {
+                            modeSet.valueOf(mode);
+                            break;
+                        } catch(IllegalArgumentException e){
+                            out.println("ERROR: Invalid mode. mode should be READ_ONLY, WRITE_ONLY, or RW");
+                            mode = in.readLine();
+                        }
+                    }
+
+
                     // 書き込みモード -> ロック管理
                     if (mode.contains("WRITE")) {
                         if (writeLocks.containsKey(path)) {
@@ -51,28 +80,66 @@ public class DFSServer {
                         writeLocks.put(path, socket.getRemoteSocketAddress().toString());
                     }
 
-                    String content = fileStore.getOrDefault(path, "");
+                    File filedata = fileStore.get(path);
+                    if(filedata == null){
+                        filedata = new File();
+                        filedata.lastaccessed = LocalDateTime.now();
+                        fileStore.put(path, filedata);// ファイル内容を保存
+                    }else{
+                        filedata.lastaccessed = LocalDateTime.now();
+                    }
+                    
                     out.println("SUCCESS");
-                    out.println(content); // 実際は終端文字などの工夫が必要
+                    out.println(filedata.content); // 実際は終端文字などの工夫が必要
 
                 } else if ("STORE".equals(command)) {
                     String path = parts[1];
                     StringBuilder content = new StringBuilder();
                     String inputLine;
+
                     while (!(inputLine = in.readLine()).equals("__END__")) {
                         content.append(inputLine).append("\n");
                     }
-                    fileStore.put(path, content.toString().trim());
+                    File filedata = fileStore.get(path);
+                    if (filedata == null) {
+                        System.out.println("Creating new file entry for: " + path);
+                        filedata = new File();
+                    }
+
+                    filedata.content = content.toString().trim();// ファイル内容
+                    filedata.lastsaved = LocalDateTime.now(); // 最終保存日時
+                     
+                    fileStore.put(path, filedata);// ファイル内容を保存
                     writeLocks.remove(path); // 更新後にロック解除
                     out.println("SUCCESS");
+
                 } else if("UNLOCK".equals(command)) {
                     String path = parts[1];
                     writeLocks.remove(path); // ロックを解除
                     out.println("SUCCESS");
-                } else if ("-ls".equals(command)) {
-                    out.println("FILES_LIST_START");
+
+                } else if ("LIST_FILES".equals(command)) {
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                   
+                    out.println("FILES_LIST_START");//
+                    out.println("File List:");
+                    out.println("Filename \t Last Saved \t\t Last Accessed");
+
                     for (String filePath : fileStore.keySet()) {
-                        out.println(filePath);
+                        out.print("- " + filePath);//ファイル名を送信
+
+                        if(fileStore.get(filePath).lastsaved != null){//最終保存日時があれば送信
+                            out.print(" \t " + fileStore.get(filePath).lastsaved.format(formatter));
+                        }else{
+                            out.print(" \t " + "***");
+                        }
+
+                        if(fileStore.get(filePath).lastaccessed != null){//最終アクセス日時があれば送信
+                            out.println(" \t " + fileStore.get(filePath).lastaccessed.format(formatter));
+                        }else{
+                            out.println(" \t " + "***");
+                        }
+
                     }
                     out.println("FILES_LIST_END");
 
